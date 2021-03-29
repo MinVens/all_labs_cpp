067e3db1322942d0c3759e3b3c63aded445d971e
MinVens <arbsolon1374@gmail.com>
2021-03-29 15:45:49 +0300

    Update Lab_2.cpp

--- Lab_2.cpp
+++ Lab_2.cpp
@@ -1,5 +1,4 @@
 /*
-Laba №2
 1. Створити програму на мові С згідно варіанту використавши середовище
 програмування Dev-C++ 4.0:
 *за допомогою текстового редактора створити файл, що містить текст, довжина
@@ -18,6 +17,7 @@ Laba №2
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -29,12 +29,18 @@ int main(int argc, char** argv)
     SetConsoleOutputCP(1251);
     string str;
     char path[BUFSIZ];
+    f_back:
     cout << "Введіть шлях до файлу (Приклад : D:\\\\КТП\\\\l2.dat) :\n";
     cin >> path;
+    if ((fopen(path, "r")) == NULL)
+    {
+        cout << "Файл не існує у данному місці, перевірте правильність шляху" << endl;
+        goto f_back;
+    }
     cout << "Текст в файлі " << path << " :\n\n"; 
     ifstream read_n_check(path, std::ios::app);
-	str.assign( (std::istreambuf_iterator<char>(read_n_check) ), (std::istreambuf_iterator<char>()	) );
-	cout << str << std::endl;
+    str.assign( (std::istreambuf_iterator<char>(read_n_check) ), (std::istreambuf_iterator<char>()  ) );
+    cout << str << std::endl;
     cout << "\nСимвол, що зустрічається максмальну кількість разів : " << mostUsed(str);
     read_n_check.close();
     return 0;
@@ -44,21 +50,21 @@ char mostUsed(string str)
 {
     vector<char> letters;
     for (int i = 0; i < str.size(); i++) 
-	{
+    {
         if (i == 0) 
-		{
+        {
             letters.push_back(str[i]);
             continue;
         }
         int count = 0;
         for (int j = 0; j < letters.size(); j++) 
-		{
+        {
             if (str[i] == letters[j])
                 break;
             else 
-			{
+            {
                 for (int k = 0; k < letters.size(); k++) 
-				{
+                {
                     if (str[i] != letters[k])
                         count++;
                 }
@@ -69,18 +75,18 @@ char mostUsed(string str)
     }
     vector<int> occur(letters.size());
     for (int i = 0; i < str.size(); i++) 
-	{
+    {
         for (int j = 0; j < letters.size(); j++) 
-		{
+        {
             if (str[i] == letters[j])
                 occur[j]++;
         }
     }
     int maxAmount = -1, maxIndex = -1;
     for (int i = 0; i < letters.size(); i++) 
-	{
+    {
         if (occur[i] > maxAmount) 
-		{
+        {
             maxAmount = occur[i];
             maxIndex = i;
         }
